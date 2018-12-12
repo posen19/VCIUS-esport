9d816ff7a0c55b488dddb4248cd37f304b59901b
posen <diaspaulo19@gmail.com>
2018-12-12 16:25:05 +0100

    Eliminar por comando + crear historico de lo eliminado.

--- events.js
+++ events.js
@@ -13,6 +13,45 @@ function messageHandler(message, client) {
   let cmd;
 
   let commandHandlerCheck = client.config.prefixSeperated ? args[0].toLowerCase() === client.config.prefix.toLowerCase() : message.content.startsWith(client.config.prefix.toLowerCase())
+
+  //Eliminar mensajes
+  if (commandHandlerCheck && args[1] === "del") {  //miramos si el comando es "del"
+    console.log("Se procede a eliminar las lineas");
+    if (message.member.roles.find("name","VBoss") || message.member.roles.find("name","TeamZ")) {
+      let num = parseInt(args[2]);
+      if (isNaN(num) || num > 10 || num < 1) {
+        const time = client.moment().format('MMMM Do YYYY, HH:mm:ss');
+        const guild = message.guild ? `${message.guild.name} (${message.guild.id})` : `Private Message`
+        console.log(`>>>> Command called [${time}]`);
+        console.log(`   > Guild: ${guild}`);
+        console.log(`   > User: ${message.author.tag} (${message.author.id})`);
+        console.log(`   > Content: ${message.cleanContent}`);
+        console.log("   > Comando inválido.");
+        return;
+      }
+
+      message.channel.fetchMessages({limit:num})
+      .then(message => {
+        let messageArr = message.array();
+        let messageCount = messageArr.length;
+
+        for(let i = messageCount-1; i>=0; i--) {
+          client.channels.find("name","historiaborrada").send("El usuario " + messageArr[i].author.username + " escribió:\n" + messageArr[i].cleanContent + "\n" + messageArr[i].createdAt);
+          messageArr[i].delete()
+          .catch(function(err) {
+            console.log("Ha ocurrido un erro al eliminar las lineas");
+            console.log(err);
+          })
+        }
+      })
+      .catch(function(err) {
+        console.log("Ha ocurrido un error al recuperar los mensajes");
+        console.log(err);
+      })
+    }
+    return;
+  }
+
   if (commandHandlerCheck) {
     cmd = client.config.prefixSeperated ? args[1] : args[0].substring(client.config.prefix.length);
 
